b670d82cbc524f2b68ee523f92af1d405fcb7993
James Whitfield <jwhitfield@neilab.com>
2013-10-25 14:57:24 -0400

    added helpers to Cms::User model

--- lib/bcms_ldap_auth/authentication.rb
+++ lib/bcms_ldap_auth/authentication.rb
@@ -2,6 +2,15 @@ module Cms
 
   User.class_eval do
 
+    include Adauth::Rails::ModelBridge
+
+    AdauthMappings = {
+        :login => :login,
+        :group_strings => :cn_groups
+    }
+
+    AdauthSearchField = [:login, :login]
+
     def self.authenticate_with_ldap(login, password)
       u = authenticate_without_ldap(login, password)
       u
