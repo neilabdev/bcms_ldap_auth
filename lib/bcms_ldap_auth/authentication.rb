7281394ab762a46e0bc8b595ee6a72ecaf1f9869
James Whitfield <jwhitfield@neilab.com>
2013-10-26 11:20:07 -0400

    add group synchronization

--- lib/bcms_ldap_auth/authentication.rb
+++ lib/bcms_ldap_auth/authentication.rb
@@ -6,7 +6,6 @@ module Cms
     include Adauth::Rails::ModelBridge
 
 
-
     def self.authenticate_with_ldap(login, password)
       u = authenticate_without_ldap(login, password)
       return u unless u.nil?
@@ -17,8 +16,8 @@ module Cms
         if ldap_user then
           u = self.return_and_create_from_adauth(ldap_user)
           u.sync_group = ldap_user.groups.andand.collect { |g| g.name }
-          u.password=u.password_confirmation=password  # Remembers last successful password in LDAP. Should we make it unusable so that LDAP users only auth from LDAP?
-          #return nil unless u.save
+          u.password=u.password_confirmation=password # Remembers last successful password in LDAP. Should we make it unusable so that LDAP users only auth from LDAP?
+                                                      #return nil unless u.save
           unless u.save
             return nil
           end
@@ -31,21 +30,38 @@ module Cms
 
     def sync_group=(group_names)
       return unless group_names.is_a?(Array)
-      ldap_group_type = nil
-      user_groups = []
+      self.transaction do |t|
+        ldap_group_type =nil
+        user_groups = nil
 
-      group_names.each do |name|
-        # Add or Create groups user not already associated with
-        ldap_group_type ||= Cms::GroupType.where({:name => "LDAP User"}).first
-        user_groups ||= self.groups.collect { |g| g.name }
-        ldap_group = Cms::Group.where({:name => name, :code => "ldap"}).first
-        ldap_group =  Cms::Group.create!(:name => name, :code => "ldap", :group_type_id => ldap_group_type.id) if ldap_group.nil?
 
-        unless user_groups.include? ldap_group.name then
-          self.groups << ldap_group
+
+        group_names.each do |name|
+          # Add or Create groups user not already associated with
+          ldap_group_type ||= Cms::GroupType.where({:name => "LDAP User"}).first
+          user_groups ||= self.groups.collect { |g| g.name }
+          ldap_group = Cms::Group.where({:name => name, :code => "ldap"}).first
+          ldap_group = Cms::Group.create!(:name => name, :code => "ldap", :group_type_id => ldap_group_type.id) if ldap_group.nil?
+
+          unless user_groups.include? ldap_group.name then
+            self.groups << ldap_group
+          end
         end
-      end
 
+        if group_names.empty? then
+          # remove all ldap entries if they are empty in LDAP
+
+        end
+
+        user_groups.andand.each do |name|
+          unless group_names.include? name then
+            # remove associateion that has been deleted in ldap
+            # TODO: Test this condition
+            remove_group = Cms::Group.where({:name => name, :code => "ldap"}).first
+            self.groups.delete(remove_group) unless remove_group.nil?
+          end
+        end #user_groups
+      end
 
     end
 
